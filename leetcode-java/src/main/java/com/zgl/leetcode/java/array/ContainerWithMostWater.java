d0113f37c8f2e3261e6ad5ae2e041a2f9e169d4c
zgl <285683264@qq.com>
2019-04-03 20:03:23 +0800

    add 41 and 42 solution

--- leetcode-java/src/main/java/com/zgl/leetcode/java/array/ContainerWithMostWater.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/array/ContainerWithMostWater.java
@@ -7,24 +7,24 @@ package com.zgl.leetcode.java.array;
 public class ContainerWithMostWater {
 	/**
 	 * 11. Container With Most Water
-	 *
+	 * <p>
 	 * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
-	 *
+	 * <p>
 	 * Note: You may not slant the container and n is at least 2.
 	 * Example:
-	 *
+	 * <p>
 	 * Input: [1,8,6,2,5,4,8,3,7]
 	 * Output: 49
 	 */
 	public int maxArea(int[] height) {
-		int left=0,right=height.length-1;
-		int max=Math.min(height[left],height[right])*(right-left);
-		while(left<right){
-			if(height[left]<height[right]){
-				max=Math.max(max,height[left]*(right-left));
+		int left = 0, right = height.length - 1;
+		int max = Math.min(height[left], height[right]) * (right - left);
+		while (left < right) {
+			if (height[left] < height[right]) {
+				max = Math.max(max, height[left] * (right - left));
 				left++;
-			}else {
-				max=Math.max(max,height[right]*(right-left));
+			} else {
+				max = Math.max(max, height[right] * (right - left));
 				right--;
 			}
 		}
@@ -32,8 +32,8 @@ public class ContainerWithMostWater {
 	}
 
 	public static void main(String[] args) {
-		ContainerWithMostWater mock=new ContainerWithMostWater();
-		int[]height={1,8,6,2,5,4,8,3,7};
+		ContainerWithMostWater mock = new ContainerWithMostWater();
+		int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
 		System.out.println(mock.maxArea(height));
 	}
 }
