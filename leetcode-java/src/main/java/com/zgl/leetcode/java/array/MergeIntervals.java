ac31643e6d472cc07fbf1d21c00816c759d3534b
zgl <zhangguangling@100tal.com>
2019-04-22 15:05:45 +0800

    add 56. Merge Intervals solution

---
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/array/MergeIntervals.java
@@ -0,0 +1,86 @@
+package com.zgl.leetcode.java.array;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author zgl
+ * @date 2019/4/20 下午3:44
+ */
+public class MergeIntervals {
+	/**
+	 * 56. Merge Intervals
+	 * Given a collection of intervals, merge all overlapping intervals.
+	 * <p>
+	 * Example 1:
+	 * <p>
+	 * Input: [[1,3],[2,6],[8,10],[15,18]]
+	 * Output: [[1,6],[8,10],[15,18]]
+	 * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+	 * Example 2:
+	 * <p>
+	 * Input: [[1,4],[4,5]]
+	 * Output: [[1,5]]
+	 * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+	 */
+	public int[][] merge(int[][] intervals) {
+		if (intervals.length == 0){
+			return intervals;
+		}
+		List<List<Integer>> nums = arrayToList(intervals);
+		List<List<Integer>> result = new ArrayList<>();
+		nums.sort(Comparator.comparingInt(list -> list.get(0)));
+		List<Integer> temp = nums.get(0);
+		if (nums.size() == 1) {
+			result.add(temp);
+			return listToArray(nums);
+		}
+		for (int i = 1; i < nums.size(); i++) {
+			if (temp.get(1) >= nums.get(i).get(0)) {
+				temp.set(1, Math.max(temp.get(1), nums.get(i).get(1)));
+			} else {
+				result.add(temp);
+				temp = nums.get(i);
+			}
+		}
+		result.add(temp);
+		return listToArray(result);
+	}
+
+	private List<List<Integer>> arrayToList(int[][] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> row;
+		for (int i = 0; i < nums.length; i++) {
+			row = new ArrayList<>();
+			for (int j = 0; j < nums[i].length; j++) {
+				row.add(nums[i][j]);
+			}
+			result.add(row);
+		}
+		return result;
+	}
+
+	private int[][] listToArray(List<List<Integer>> nums) {
+		int length = nums.get(0).size();
+		int width = nums.size();
+		int[][] result = new int[width][length];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < length; j++) {
+				result[i][j] = nums.get(i).get(j);
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[][] nums = {{1, 4}, {0, 4}};
+		int[][] res = new MergeIntervals().merge(nums);
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res[i].length; j++) {
+				System.out.println(res[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
--- leetcode-java/src/main/java/com/zgl/leetcode/java/dynamic/LCS.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/dynamic/LCS.java
@@ -2,27 +2,28 @@ package com.zgl.leetcode.java.dynamic;
 
 /**
  * 最长公共子串
+ *
  * @author zgl
  * @date 2018/12/8 下午6:46
  */
 public class LCS {
-	public int lengthOfLCS(String s1,String s2){
+	public int lengthOfLCS(String s1, String s2) {
 		/**
 		 * dp[i][j]表示s1第i个元素和s2第j个元素的最长公共子串
 		 */
-		int[][]dp=new int[s1.length()+1][s2.length()+1];
-		for(int i=1;i<=s1.length();i++){
-			for(int j=1;j<=s2.length();j++){
-				if(s1.charAt(i-1)==s2.charAt(j-1)){
-					dp[i][j]=dp[i-1][j-1]+1;
-				}else {
-					dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+		int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+		for (int i = 1; i <= s1.length(); i++) {
+			for (int j = 1; j <= s2.length(); j++) {
+				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
 				}
 			}
 		}
-		for(int i=0;i<=s1.length();i++){
-			for(int j=0;j<=s2.length();j++){
-				System.out.print(dp[i][j]+" ");
+		for (int i = 0; i <= s1.length(); i++) {
+			for (int j = 0; j <= s2.length(); j++) {
+				System.out.print(dp[i][j] + " ");
 			}
 			System.out.println();
 		}
@@ -30,9 +31,9 @@ public class LCS {
 	}
 
 	public static void main(String[] args) {
-		LCS mock=new LCS();
-		String s1="123456";
-		String s2="a234asfaf";
-		System.out.println(mock.lengthOfLCS(s1,s2));
+		LCS mock = new LCS();
+		String s1 = "123456";
+		String s2 = "a234asfaf";
+		System.out.println(mock.lengthOfLCS(s1, s2));
 	}
 }
