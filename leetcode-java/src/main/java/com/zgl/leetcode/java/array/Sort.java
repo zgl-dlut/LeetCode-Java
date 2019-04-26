b4bfd68370c5e09bedf38e0fd7c3c7374cc04ab9
zgl <285683264@qq.com>
2019-04-26 19:37:26 +0800

    add selectSort maxHeapSort and advancedBubbleSort algorithm

--- leetcode-java/src/main/java/com/zgl/leetcode/java/array/Sort.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/array/Sort.java
@@ -9,19 +9,14 @@ import java.util.*;
 public class Sort {
 	/**
 	 * 冒泡排序
-	 *
-	 * @param numbers
-	 * @return
 	 */
 	public int[] bubbleSort(int[] numbers) {
 		int length = numbers.length;
-		int i, j, temp;
+		int i, j;
 		for (i = 0; i < length - 1; i++) {
 			for (j = 0; j < length - 1 - i; j++) {
 				if (numbers[j] > numbers[j + 1]) {
-					temp = numbers[j];
-					numbers[j] = numbers[j + 1];
-					numbers[j + 1] = temp;
+					swap(numbers, j, j + 1);
 				}
 			}
 			for (int k : numbers) {
@@ -34,21 +29,53 @@ public class Sort {
 
 	/**
 	 * 改进版冒泡排序
-	 *
-	 * @param numbers
-	 * @return
 	 */
 	public int[] bubbleSortAdvanced(int[] numbers) {
+		int start = 0, end = numbers.length;
+		while (start < end){
+			for(int i = start; i < end - 1; i++){
+				if(numbers[i] > numbers[i + 1]){
+					swap(numbers, i, i + 1);
+				}
+			}
+			end--;
+			for(int i = end; i > start; i--){
+				if(numbers[i] < numbers[i - 1]){
+					swap(numbers, i - 1, i);
+				}
+			}
+			start++;
+			for (int k : numbers) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
 		return numbers;
 	}
 
+	private void swap(int[] numbers, int i, int j){
+		int temp = numbers[i];
+		numbers[i] = numbers[j];
+		numbers[j] = temp;
+	}
+
+	/**
+	 * 简单选择排序
+	 */
+	public int[] selectSort(int[] numbers) {
+		for (int i = 0; i < numbers.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < numbers.length; j++) {
+				if (numbers[i] > numbers[j]) {
+					min = j;
+				}
+			}
+			swap(numbers, i, min);
+		}
+		return numbers;
+	}
 	/**
 	 * 快速排序
-	 *
-	 * @param numbers
-	 * @param left
-	 * @param right
-	 * @return
 	 */
 	public int[] quickSort(int[] numbers, int left, int right) {
 		if (left < right) {
@@ -61,11 +88,8 @@ public class Sort {
 
 	/**
 	 * partition求解
-	 *
-	 * @param numbers
-	 * @return
 	 */
-	public int partition(int[] numbers, int left, int right) {
+	private int partition(int[] numbers, int left, int right) {
 		int pivot = numbers[left];
 		while (left < right) {
 			while (numbers[right] >= pivot && left < right) {
@@ -87,11 +111,6 @@ public class Sort {
 
 	/**
 	 * 归并排序
-	 *
-	 * @param numbers
-	 * @param left
-	 * @param right
-	 * @return
 	 */
 	public int[] mergeSort(int[] numbers, int left, int right) {
 		int[] temp = new int[numbers.length];
@@ -107,7 +126,7 @@ public class Sort {
 	/**
 	 * 归并排序分治
 	 */
-	public void mergePartition(int[] numbers, int left, int middle, int right, int[] temp) {
+	private void mergePartition(int[] numbers, int left, int middle, int right, int[] temp) {
 		int i = left;
 		int tag = 0;
 		int j = middle + 1;
@@ -118,11 +137,6 @@ public class Sort {
 			while (j <= right && numbers[i] > numbers[j]) {
 				temp[tag++] = numbers[j++];
 			}
-			/*if(numbers[i]<=numbers[j]){
-				temp[tag++]=numbers[i++];
-			}else {
-				temp[tag++]=numbers[j++];
-			}*/
 		}
 		while (i <= middle) {
 			temp[tag++] = numbers[i++];
@@ -140,12 +154,78 @@ public class Sort {
 		System.out.println();
 	}
 
+	/**
+	 * 将元素array[k]自下往上逐步调整树形结构
+	 */
+	private void adjustDownToUp(int[] array, int k, int length){
+		int temp = array[k];
+		/**
+		 * i为初始化为节点k的左孩子，沿节点较大的子节点向下调整
+		 */
+		for(int i = 2 * k + 1; i < length - 1; i = 2 * i + 1){
+			if(i + 1 < length && array[i] < array[i + 1]){
+				i++;
+			}
+			/**
+			 * 根节点 >=左右子女中关键字较大者，调整结束
+			 */
+			if (temp >= array[i]){
+				break;
+			}else {
+				/**
+				 * 将左右子结点中较大值array[i]调整到双亲节点上
+				 */
+				array[k] = array[i];
+				/**
+				 * 修改k值，以便继续向下调整
+				 */
+				k = i;
+			}
+		}
+		array[k] = temp;
+	}
+
+	/**
+	 * 构建大根堆
+	 */
+	private int [] buildMaxHeap(int[] array){
+		/**
+		 * 从最后一个节点array.length-1的父节点（array.length-1-1）/2开始，直到根节点0，反复调整堆
+		 */
+		for(int i = (array.length - 2) / 2; i >= 0; i--){
+			adjustDownToUp(array, i, array.length);
+		}
+		return array;
+	}
+
+	/**
+	 * 堆排序
+	 */
+	public int[] heapSort(int[] array){
+		array = buildMaxHeap(array);
+		for(int i = array.length - 1; i > 0; i--){
+			int temp = array[0];
+			array[0] = array[i];
+			array[i] = temp;
+			adjustDownToUp(array, 0, i);
+			for (int num : array) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+		}
+		return array;
+	}
+
 	public static void main(String[] args) {
 		Sort mock = new Sort();
 		int[] numbers = {49, 38, 65, 97, 76, 13, 27, 49};
-		//int[] quickResult = mock.quickSort(numbers, 0, 7);
-		//int[] buddleResult = mock.bubbleSort(numbers);
-		//int []mergeResult=mock.mergeSort(numbers,0,7);
+		//mock.quickSort(numbers, 0, 7);
+		//System.out.println("传统冒泡排序");
+		//mock.bubbleSort(numbers);
+		//System.out.println("改进冒泡排序");
+		//mock.bubbleSortAdvanced(numbers);
+		//mock.mergeSort(numbers,0,7);
+		mock.heapSort(numbers);
 		Map<Integer, Integer> orderMap = new HashMap<>();
 		orderMap.put(49, 1);
 		orderMap.put(38, 2);
@@ -160,6 +240,6 @@ public class Sort {
 		list.sort((Integer o1, Integer o2)-> o2-o1);*/
 
 		list.sort(Comparator.comparingInt(o1 -> orderMap.get(o1)));
-		list.stream().forEach(i -> System.out.println(i));
+		//list.stream().forEach(i -> System.out.println(i));
 	}
 }
