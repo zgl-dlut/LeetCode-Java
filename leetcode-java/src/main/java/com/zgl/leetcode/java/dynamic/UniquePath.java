7c1f24b1cfab645456446c0e0b3450f392c4791c
zgl <285683264@qq.com>
2019-04-28 14:24:08 +0800

    add 63 Unique Paths and 64. Minimum Path Sum solutions with dynamic programming

--- leetcode-java/src/main/java/com/zgl/leetcode/java/dynamic/UniquePath.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/dynamic/UniquePath.java
@@ -46,4 +46,107 @@ public class UniquePath {
 		}
 		return dp[m - 1][n - 1];
 	}
+
+	/**
+	 * 63. Unique Paths II
+	 * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+	 *
+	 * The robot can only move either down or right at any point in time.
+	 * The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+	 *
+	 * Now consider if some obstacles are added to the grids. How many unique paths would there be?
+	 *
+	 * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+	 *
+	 * Note: m and n will be at most 100.
+	 *
+	 * Example 1:
+	 *
+	 * Input:
+	 * [
+	 *   [0,0,0],
+	 *   [0,1,0],
+	 *   [0,0,0]
+	 * ]
+	 * Output: 2
+	 * Explanation:
+	 * There is one obstacle in the middle of the 3x3 grid above.
+	 * There are two ways to reach the bottom-right corner:
+	 * 1. Right -> Right -> Down -> Down
+	 * 2. Down -> Down -> Right -> Right
+	 */
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		int m = obstacleGrid.length;
+		int n = obstacleGrid[0].length;
+		int[][] dp = new int[m][n];
+		dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+		for (int i = 1; i < m; i++) {
+			dp[i][0] = obstacleGrid[i][0] == 1 ? 0 : dp[i - 1][0];
+		}
+		for (int i = 1; i < n; i++) {
+			dp[0][i] = obstacleGrid[0][i] == 1 ? 0 : dp[0][i - 1];
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = obstacleGrid[i][j] == 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+
+	/**
+	 * 64. Minimum Path Sum
+	 * Given a m x n grid filled with non-negative numbers,
+	 * find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+	 *
+	 * Note: You can only move either down or right at any point in time.
+	 *
+	 * Example:
+	 *
+	 * Input:
+	 * [
+	 *   [1,3,1],
+	 *   [1,5,1],
+	 *   [4,2,1]
+	 * ]
+	 * Output: 7
+	 * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+	 */
+	public int minPathSum(int[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
+		int[][] dp = new int[m][n];
+		dp[0][0] = grid[0][0];
+		for (int i = 1; i < m; i++) {
+			dp[i][0] = grid[i][0] + dp[i - 1][0];
+		}
+		for (int i = 1; i < n; i++) {
+			dp[0][i] = grid[0][i] + dp[0][i - 1];
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+
+	/**
+	 * O(n)空间复杂度
+	 */
+	public int minPathSum1(int[][] grid){
+	 	int m = grid.length;
+	 	int n = grid[0].length;
+	 	int[] dp = new int[m];
+	 	dp[0] = grid[0][0];
+	 	for (int i = 1; i < m; i++){
+	 		dp[i] = grid[i][0] + dp[i - 1];
+	    }
+	    for (int j = 1; j < n; j++){
+	 		for (int i = 0; i < m; i++){
+	 			dp[i] = (i == 0 ? dp[i] : Math.min(dp[i], dp[i - 1])) + grid[i][j];
+		    }
+	    }
+	    return dp[m - 1];
+	}
 }
