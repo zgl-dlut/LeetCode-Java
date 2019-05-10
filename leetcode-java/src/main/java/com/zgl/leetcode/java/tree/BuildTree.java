b17df411547e020122b326ce64aea3b791958c50
zgl <285683264@qq.com>
2019-05-10 17:05:26 +0800

    add tree and divided two numbers solutions

--- leetcode-java/src/main/java/com/zgl/leetcode/java/tree/BuildTree.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/tree/BuildTree.java
@@ -24,7 +24,7 @@ public class BuildTree {
 		}
 		int rootVal = preorder[preStart];
 		TreeNode root = new TreeNode(rootVal);
-		if(preStart==preEnd){
+		if (preStart == preEnd) {
 			return root;
 		}
 		int index = 0;
@@ -55,7 +55,7 @@ public class BuildTree {
 		}
 		int rootVal = postorder[postEnd];
 		TreeNode root = new TreeNode(rootVal);
-		if(postStart==postEnd){
+		if (postStart == postEnd) {
 			return root;
 		}
 		int index = 0;
@@ -66,42 +66,43 @@ public class BuildTree {
 			}
 		}
 		int leftLength = index - inStart;
-		root.left = builder2(inorder, inStart, index - 1, postorder, postStart, postStart + leftLength-1);
-		root.right = builder2(inorder, index + 1, inEnd, postorder, postStart + leftLength , postEnd-1);
+		root.left = builder2(inorder, inStart, index - 1, postorder, postStart, postStart + leftLength - 1);
+		root.right = builder2(inorder, index + 1, inEnd, postorder, postStart + leftLength, postEnd - 1);
 		return root;
 	}
 
 	/**
 	 * 889. Construct Binary Tree from Preorder and Postorder Traversal
 	 * Return any binary tree that matches the given preorder and postorder traversals.
-	 *
+	 * <p>
 	 * Values in the traversals pre and post are distinct positive integers.
 	 */
 	public TreeNode constructFromPrePost(int[] pre, int[] post) {
-		int preLength=pre.length;
-		int postLength=post.length;
-		return builder3(pre,0,preLength-1,post,0,postLength-1);
+		int preLength = pre.length;
+		int postLength = post.length;
+		return builder3(pre, 0, preLength - 1, post, 0, postLength - 1);
 	}
-	public TreeNode builder3(int[] pre, int preStart, int preEnd, int[] post, int postStart, int postEnd){
-		if(preStart>preEnd||postStart>postEnd){
+
+	public TreeNode builder3(int[] pre, int preStart, int preEnd, int[] post, int postStart, int postEnd) {
+		if (preStart > preEnd || postStart > postEnd) {
 			return null;
 		}
-		int rootValue=pre[preStart];
-		TreeNode root=new TreeNode(rootValue);
-		if (preStart==preEnd){
+		int rootValue = pre[preStart];
+		TreeNode root = new TreeNode(rootValue);
+		if (preStart == preEnd) {
 			return root;
 		}
-		int index=0;
-		int firstLeftVal=pre[preStart+1];
-		for(int i=postStart;i<=postEnd;i++){
-			if(post[i]==firstLeftVal){
-				index=i;
+		int index = 0;
+		int firstLeftVal = pre[preStart + 1];
+		for (int i = postStart; i <= postEnd; i++) {
+			if (post[i] == firstLeftVal) {
+				index = i;
 				break;
 			}
 		}
-		int leftLength=index-postStart+1;
-		root.left=builder3(pre,preStart+1,preStart+leftLength,post,postStart,index);
-		root.right=builder3(pre,preStart+leftLength+1,preEnd,post,index+1,postEnd-1);
+		int leftLength = index - postStart + 1;
+		root.left = builder3(pre, preStart + 1, preStart + leftLength, post, postStart, index);
+		root.right = builder3(pre, preStart + leftLength + 1, preEnd, post, index + 1, postEnd - 1);
 		return root;
 	}
 }
--- leetcode-java/src/main/java/com/zgl/leetcode/java/tree/Traversal.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/tree/Traversal.java
@@ -79,30 +79,150 @@ public class Traversal {
 
 	/**
 	 * 102. Binary Tree Level Order Traversal
+	 * 分层打印二叉树
 	 */
 	public List<List<Integer>> levelOrder(TreeNode root) {
-		List<List<Integer>>result=new ArrayList<>();
-		if(root==null){
+		List<List<Integer>> result = new ArrayList<>();
+		if (root == null) {
 			return result;
 		}
-		Queue<TreeNode>queue1=new LinkedList<>();
+		Queue<TreeNode> queue1 = new LinkedList<>();
 		queue1.offer(root);
-		while (!queue1.isEmpty()){
-			List<Integer>temp=new ArrayList<>();
-			Queue<TreeNode>queue2=new LinkedList<>();
-			for(TreeNode child:queue1){
-				if(child.left!=null){
+		while (!queue1.isEmpty()) {
+			List<Integer> temp = new ArrayList<>();
+			Queue<TreeNode> queue2 = new LinkedList<>();
+			for (TreeNode child : queue1) {
+				if (child.left != null) {
 					queue2.offer(child.left);
 				}
-				if(child.right!=null){
+				if (child.right != null) {
 					queue2.offer(child.right);
 				}
 				temp.add(child.val);
 			}
 			result.add(temp);
-			queue1=queue2;
+			queue1 = queue2;
 		}
 		return result;
 	}
 
+	public List<List<Integer>> levelOrder2(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+		levelRecursion(root, result, 0);
+
+		return result;
+	}
+
+	/**
+	 * 递归方法
+	 */
+	private void levelRecursion(TreeNode node, List<List<Integer>> result,
+	                            int level) {
+		if (node == null) {
+			return;
+		}
+		/**
+		 * 说明还需要添加一行
+		 */
+		if (result.size() < level + 1) {
+			result.add(new ArrayList<>());
+		}
+		result.get(level).add(node.val);
+
+		levelRecursion(node.left, result, level + 1);
+		levelRecursion(node.right, result, level + 1);
+	}
+
+	/**
+	 * 103. Binary Tree Zigzag Level Order Traversal
+	 * For example:
+	 * Given binary tree [3,9,20,null,null,15,7],
+	 *     3
+	 *    / \
+	 *   9  20
+	 *     /  \
+	 *    15   7
+	 * return its zigzag level order traversal as:
+	 * [
+	 *   [3],
+	 *   [20,9],
+	 *   [15,7]
+	 * ]
+	 */
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+		List<List<Integer>> result = new ArrayList<>();
+		if (root == null) {
+			return result;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		int count = 0;
+		while (!queue.isEmpty()){
+			count++;
+			List<Integer> temp = new ArrayList<>();
+			int n = queue.size();
+			for (int i = 0; i < n; i++){
+				TreeNode front = queue.poll();
+				temp.add(front.val);
+				if (front.left != null){
+					queue.offer(front.left);
+				}
+				if (front.right != null){
+					queue.offer(front.right);
+				}
+			}
+			if (count % 2 == 0){
+				Collections.reverse(temp);
+			}
+			result.add(temp);
+		}
+		return result;
+	}
+
+	/**
+	 * 107. Binary Tree Level Order Traversal II
+	 * Given a binary tree, return the bottom-up level order traversal of its nodes' values.
+	 * (ie, from left to right, level by level from leaf to root).
+	 *
+	 * For example:
+	 * Given binary tree [3,9,20,null,null,15,7],
+	 *     3
+	 *    / \
+	 *   9  20
+	 *     /  \
+	 *    15   7
+	 * return its bottom-up level order traversal as:
+	 * [
+	 *   [15,7],
+	 *   [9,20],
+	 *   [3]
+	 * ]
+	 */
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (root == null){
+			return result;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		while (!queue.isEmpty()){
+			int n = queue.size();
+			List<Integer> temp = new ArrayList<>();
+			for (int i = 0; i < n; i++){
+				TreeNode front = queue.poll();
+				temp.add(front.val);
+				if (front.left != null){
+					queue.offer(front.left);
+				}
+				if (front.right != null){
+					queue.offer(front.right);
+				}
+			}
+			result.add(temp);
+		}
+		Collections.reverse(result);
+		return result;
+	}
 }
