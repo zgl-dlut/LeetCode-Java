f558522c2b6eaa66f9ed722ad186a3244686ef69
zgl <285683264@qq.com>
2019-05-24 16:53:18 +0800

    add House Robber2 using dynamic programming

--- leetcode-java/src/main/java/com/zgl/leetcode/java/dynamic/HouseRobber.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/dynamic/HouseRobber.java
@@ -29,7 +29,7 @@ public class HouseRobber {
 	 * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
 	 *              Total amount you can rob = 2 + 9 + 1 = 12.
 	 */
-	public int rob(int[] nums) {
+	public int rob1(int[] nums) {
 		int length = nums.length;
 		if (length == 0) {
 			return 0;
@@ -51,8 +51,56 @@ public class HouseRobber {
 		return result;
 	}
 
+	/**
+	 * 213. House Robber II
+	 * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed.
+	 * All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one.
+	 * Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+	 *
+	 * Given a list of non-negative integers representing the amount of money of each house,
+	 * determine the maximum amount of money you can rob tonight without alerting the police.
+	 *
+	 * Example 1:
+	 *
+	 * Input: [2,3,2]
+	 * Output: 3
+	 * Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+	 *              because they are adjacent houses.
+	 * Example 2:
+	 *
+	 * Input: [1,2,3,1]
+	 * Output: 4
+	 * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+	 *              Total amount you can rob = 1 + 3 = 4.
+	 */
+	public int rob(int[] nums) {
+		int length = nums.length;
+		if (length == 0) {
+			return 0;
+		}
+		if (length == 1) {
+			return nums[0];
+		}
+		if (length == 2) {
+			return Math.max(nums[0], nums[1]);
+		}
+		return Math.max(subRob(nums, 0), subRob(nums, 1));
+	}
+
+	private int subRob(int[] nums, int start) {
+		int[] dp = new int[nums.length - 1];
+		dp[0] = nums[start];
+		dp[1] = Math.max(nums[start], nums[start + 1]);
+		int result = dp[1];
+		for (int i = 2; i < nums.length - 1; i++) {
+			dp[i] = Math.max(dp[i - 1], nums[i + start] + dp[i - 2]);
+			result = Math.max(dp[i], result);
+		}
+		return result;
+	}
+
 	public static void main(String[] args) {
-		int[] nums = {2,1,1,2};
+		int[] nums = {2,3,2,1,3};
 		System.out.println(new HouseRobber().rob(nums));
 	}
 }
