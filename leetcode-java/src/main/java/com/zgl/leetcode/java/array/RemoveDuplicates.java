e6a1d1236b2ac3852758d42f4b859f102885c7c3
zgl <285683264@qq.com>
2019-06-27 17:36:04 +0800

    solve two questions

--- leetcode-java/src/main/java/com/zgl/leetcode/java/array/RemoveDuplicates.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/array/RemoveDuplicates.java
@@ -32,7 +32,7 @@ public class RemoveDuplicates {
 	 *
 	 * It doesn't matter what values are set beyond the returned length.
 	 */
-	public int removeDuplicates(int[] nums) {
+	public int removeDuplicates1(int[] nums) {
 		if (nums == null || nums.length == 0) {
 			return 0;
 		}
@@ -40,6 +40,43 @@ public class RemoveDuplicates {
 		for (int i = 1; i < nums.length; i++) {
 			if (nums[i] != nums[index]) {
 				nums[++index] = nums[i];
+			}
+		}
+		return index + 1;
+	}
+
+	/**
+	 * 80. Remove Duplicates from Sorted Array II
+	 * Example 1:
+	 *
+	 * Given nums = [1,1,1,2,2,3],
+	 *
+	 * Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+	 *
+	 * It doesn't matter what you leave beyond the returned length.
+	 * Example 2:
+	 *
+	 * Given nums = [0,0,1,1,1,1,2,3,3],
+	 *
+	 * Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+	 *
+	 * It doesn't matter what values are set beyond the returned length.
+	 */
+	public int removeDuplicates(int[] nums) {
+		int length = nums.length;
+		if (length <= 2) {
+			return length;
+		}
+		int count = 1, index = 0;
+		for (int i = 1; i < length; i++) {
+			if (nums[i] != nums[i - 1]) {
+				count = 1;
+				nums[++index] = nums[i];
+			} else {
+				if (count < 2) {
+					nums[++index] = nums[i];
+					count++;
+				}
 			}
 		}
 		return index + 1;
--- leetcode-java/src/main/java/com/zgl/leetcode/java/array/SearchInRotatedSortedArray.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/array/SearchInRotatedSortedArray.java
@@ -5,6 +5,11 @@ package com.zgl.leetcode.java.array;
  * @date 2019/1/3 下午7:17
  */
 public class SearchInRotatedSortedArray {
+	public static void main(String[] args) {
+		int[] nums = {3,1};
+		System.out.println(new SearchInRotatedSortedArray().search(nums, 1));
+	}
+
 	/**
 	 * 33. Search in Rotated Sorted Array
 	 * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
@@ -26,27 +31,68 @@ public class SearchInRotatedSortedArray {
 	 * Input: nums = [4,5,6,7,0,1,2], target = 3
 	 * Output: -1
 	 */
-	public int search(int[] nums, int target) {
-		int left=0,right=nums.length-1,mid;
-		while(left<=right){
-			mid=left+(right-left)/2;
-			if(nums[mid]==target){
+	public int search1(int[] nums, int target) {
+		int left = 0, right = nums.length - 1, mid;
+		while (left <= right) {
+			mid = left + (right - left) / 2;
+			if (nums[mid] == target) {
 				return mid;
 			}
-			if(nums[left]<=nums[mid]){
-				if(target>=nums[left]&&target<=nums[mid]){
-					right=mid-1;
-				}else {
-					left=mid+1;
+			if (nums[left] <= nums[mid]) {
+				if (target >= nums[left] && target <= nums[mid]) {
+					right = mid - 1;
+				} else {
+					left = mid + 1;
 				}
-			}else {
-				if(target>=nums[mid]&&target<=nums[right]){
-					left=mid+1;
-				}else {
-					right=mid-1;
+			} else {
+				if (target >= nums[mid] && target <= nums[right]) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
 				}
 			}
 		}
 		return -1;
+	}
+
+	/**
+	 * 81. Search in Rotated Sorted Array II
+	 * Example 1:
+	 *
+	 * Input: nums = [2,5,6,0,0,1,2], target = 0
+	 * Output: true
+	 * Example 2:
+	 *
+	 * Input: nums = [2,5,6,0,0,1,2], target = 3
+	 * Output: false
+	 * Follow up:
+	 *
+	 * This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+	 * Would this affect the run-time complexity? How and why?
+	 */
+	public boolean search(int[] nums, int target) {
+		int left = 0, right = nums.length - 1;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			if (nums[mid] == target) {
+				return true;
+			}
+			if (nums[left] == nums[mid]) {
+				left++;
+			} else if (nums[left] < nums[mid]) {
+				if (target >= nums[left] && target < nums[mid]) {
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+			} else {
+				if (target > nums[mid] && target <= nums[right]) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
+		}
+		return false;
 	}
 }
