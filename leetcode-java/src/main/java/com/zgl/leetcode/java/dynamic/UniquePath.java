66a8aa0a9cbb264bc0e2ab5d07c8f1d5a8b1b1c9
zgl <285683264@qq.com>
2019-09-20 15:30:30 +0800

    dp二刷

--- leetcode-java/src/main/java/com/zgl/leetcode/java/dynamic/UniquePath.java
+++ leetcode-java/src/main/java/com/zgl/leetcode/java/dynamic/UniquePath.java
@@ -4,7 +4,7 @@ package com.zgl.leetcode.java.dynamic;
  * @author zgl
  * @date 2019/4/26 上午11:35
  */
-public class UniquePath {
+public abstract class UniquePath {
 
 	/**
 	 * 62. Unique Paths
@@ -148,5 +148,24 @@ public class UniquePath {
 		    }
 	    }
 	    return dp[m - 1];
+	}
+	private int uniquePathsWithObstacles1(int[][] obstacleGrid) {
+		int m = obstacleGrid.length;
+		int n = obstacleGrid[0].length;
+		int[] dp = new int[m];
+		dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+		for (int i = 1; i < m; i++) {
+			dp[i] = obstacleGrid[i][0] == 1 ? 0 : dp[i - 1];
+		}
+		for (int j = 1; j < n; j++) {
+			for (int i = 0; i < m; i++) {
+				if (i == 0) {
+					dp[i] = obstacleGrid[i][j] == 1 ? 0 : dp[i];
+				} else {
+					dp[i] = obstacleGrid[i][j] == 1 ? 0 : dp[i - 1] + dp[i];
+				}
+			}
+		}
+		return dp[m - 1];
 	}
 }
